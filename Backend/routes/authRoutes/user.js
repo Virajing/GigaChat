16602f9ee4c7b60e1842f43f210e608f1c385c26
virajing <bdsharma.869@gmail.com>
2025-09-21 09:51:47 +0530

    post route done

--- Backend/routes/authRoutes/user.js
+++ Backend/routes/authRoutes/user.js
@@ -2,13 +2,13 @@ const express = require("express");
 const router = express.Router();
 const User = require("../../models/user");
 
-router.get("/register", (req, res) => {
+router.post("/register", async (req, res) => {
     const { username, password, email } = req.body;
-    const existingUser = findOne({ username });
+    const existingUser = await User.findOne({ username });
     if (existingUser) {
         return res.status(400).json({ message: "Username already exists" });
     }
-    const newUser = User.create({ username, password, email });
+    const newUser = await User.create({ username, password, email });
     res.status(201).json({ message: "User registered successfully", user: newUser });
 });
 module.exports = router;
