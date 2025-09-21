e1ba524f10fdec283d289496a294d7a6a763c9a0
virajing <bdsharma.869@gmail.com>
2025-09-21 10:04:17 +0530

    app.jsx file uploaded

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "./Pages/HomePage.jsx";
 import Register from "./Pages/Register.jsx";
+import Login from "./Pages/Login.jsx";
 const App = () => {
   return (
 
@@ -9,6 +10,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </BrowserRouter>
   );
