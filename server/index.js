1447dbeaba9241890e95fbcc0abe197a507de6e0
virajing <bdsharma.869@gmail.com>
2026-02-11 21:17:38 +0530

    CORS policy changed

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ const PORT = process.env.PORT || 5000;
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: process.env.FRONTEND_URL || ["http://localhost:5173", "https://gigachat-three.vercel.app"],
+    origin: process.env.FRONTEND_URL || ["http://localhost:5173", "https://giga-chat-nine.vercel.app"],
     methods: ["GET", "POST"],
     credentials: true,
   },
